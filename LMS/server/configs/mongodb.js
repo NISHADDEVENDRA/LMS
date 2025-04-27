48b4c351d7fdca2cdb16cb8f6127735344edd55a
NISHADDEVENDRA <devendranishad981@gmail.com>
2025-04-27 11:58:40 +0530

    nav

--- LMS/server/configs/mongodb.js
+++ LMS/server/configs/mongodb.js
@@ -3,9 +3,13 @@ import mongoose from "mongoose";
 //connect to the mongodb database
 
 const connectDB = async () =>{
-    mongoose.connection.on('connected' ,()=> console.log('Database Connected'))
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+    try {
+        const conn =await mongoose.connect(process.env.MONGODB_URI)
+        console.log(`MongoDB connected : ${conn.connection.host}`)
+    } catch (error) {
+        console.log("Error connection to mongodb",error.message)
+        process.exit(1)
+    }
 }
 
 export default connectDB
--- LMS/server/controllers/webhooks.js
+++ LMS/server/controllers/webhooks.js
@@ -5,9 +5,9 @@ import User from "../models/User.js"
 
 export const clerkWebhooks = async (req,res)=>{
     try {
-      const whook = new Webhook(process.env.ClERK_WEBHOOK_SECRET)
+      const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
       await whook.verify(JSON.stringify(req.body),{
-        'svix-id': req.headers["svix-id"],
+        "svix-id": req.headers["svix-id"],
         "svix-timestamp":req.headers["svix-timestamp"],
         "svix-signature":req.headers["svix-signature"]
       })
@@ -18,7 +18,7 @@ export const clerkWebhooks = async (req,res)=>{
             const userData ={
                 _id :data.id,
                 email: data.email_addresses[0].email_address,
-                name:data.first_name +""+ data.last_name,
+                name:data.first_name +" "+ data.last_name,
                 imageUrl : data.image_url,
 
             }
